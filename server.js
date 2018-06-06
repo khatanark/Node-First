3710833f2a489f14f372a47d62cce64bd55c9f41
khatanark <rohitkhatana16@gmail.com>
2018-06-06 15:24:45 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express= require('express');
+const hbs= require('hbs');
+var app= express();
+
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next)=>{
+	res.render('maintainace.hbs');
+});
+
+ app.get('/', (req,res)=>{
+ 	// res.send('<h1>Hello Express!<h1>');
+ 	//  Sending Json data 
+ 	
+ 	res.send({
+ 		name:'Rohit Khatana'
+ 		// friends:['Roopal','Chahar','Saadgi','Saahil','Sakshi']
+ 	});
+ });
+
+
+app.get('/about',(req,res)=>{
+	res.render('about.hbs',
+		{
+			pageTitle: 'About Page',
+			currentYear:  new Date().getFullYear()  
+		}); 
+})
+
+ app.listen(3000);
+ console.log("Server is starting");
